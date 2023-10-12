7ea3cc3f269c8f02af8cdb24c58fa5ecb1be49e0
Michael Owuor <michael.owuor@gmail.com>
2023-10-12 18:09:18 +0300

    feat: create product category entity

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import axios from "axios";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { toast } from "react-hot-toast";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { AlertModal } from "@/components/modals/alert-modal";
+
+import { CategoryColumn } from "./columns";
+
+type CellActionProps = {
+  data: CategoryColumn;
+};
+
+export const CellAction = ({ data }: CellActionProps) => {
+  const router = useRouter();
+
+  const params = useParams();
+
+  const [open, setOpen] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  const onConfirm = async () => {
+    try {
+      setLoading(true);
+
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+
+      toast.success("The product category was deleted.");
+
+      router.refresh();
+    } catch (error) {
+      toast.error(
+        "Make sure you removed all the products in this category first.",
+      );
+    } finally {
+      setOpen(false);
+      setLoading(false);
+    }
+  };
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Product category copied to clipboard.");
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className="mr-2 h-4 w-4" />
+            Copy Category ID
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/${params.storeId}/categories/${data.id}`)
+            }
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            Update Product Category
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="mr-2 h-4 w-4" />
+            Delete Product Category
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
