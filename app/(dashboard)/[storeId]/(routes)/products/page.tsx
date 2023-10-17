01bed0d2736f7d4216117d4671418d692a7896e9
Michael Owuor <michael.owuor@gmail.com>
2023-10-17 15:19:07 +0300

    feat: create product entity (#34)

---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,49 @@
+import { format } from "date-fns";
+
+import { formatter } from "@/lib/utils/formatter";
+import prismadb from "@/lib/utils/prismadb";
+
+import { ProductClient } from "./components/client";
+import { ProductColumn } from "./components/columns";
+
+type ProductsPageProps = {
+  params: { storeId: string };
+};
+
+const ProductsPage = async ({ params }: ProductsPageProps) => {
+  const products = await prismadb.product.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      category: true,
+      size: true,
+      color: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedProducts: ProductColumn[] = products.map((item) => ({
+    id: item.id,
+    name: item.name,
+    price: formatter.format(item.price.toNumber()),
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    category: item.category.name,
+    size: item.size.name,
+    color: item.color.value,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductClient data={formattedProducts} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
